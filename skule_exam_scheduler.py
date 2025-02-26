999679544e03bc7dac5ef9094a9ff93639b47dc4
Yiyi Xu <xuyiyi0516@gmail.com>
2025-02-26 16:52:20 -0500

    added W25 exams

--- skule_exam_scheduler.py
+++ skule_exam_scheduler.py
@@ -65,11 +65,11 @@ def create_exam_calendar(selected_courses, user_last_name):
 # Streamlit UI
 st.title("SKULE Exam Calendar Export Tool")
 st.markdown("""**Note**: This tool provides exam schedules based on the latest pulled data. The tool might also not function properly for some last names on the cutoff. Please always cross-check with the [official registrar page](https://app.powerbi.com/groups/me/reports/b497d2ec-e0c4-425d-811e-0fe79b28d68d/ReportSection?experience=power-bi) to ensure accuracy. I am not liable for your missed exams!""")
-st.markdown("""**Last data pulled**: 11/8/2024 11:07:52 AM""")
+st.markdown("""**Last data pulled**: 2/26/2025 4:49:59 PM""")
 
 
 # Load the exam data from the xlsx file
-exam_data = pd.read_excel('data.xlsx').iloc[:-2]
+exam_data = pd.read_excel('dataW25.xlsx').iloc[:-2]
 
 # User input for last name
 user_last_name = st.text_input("Enter your last name:")
